692c49ac01150d855ff3af55ed71045426b0f66c
Brent Dearth <brent@pedago.com>
2015-07-02 14:45:15 -0400

    fix(ios): fix broken iOS market links by using itunes.apple.com instead of itunes.com

--- src/ios/CDVMarket.m
+++ src/ios/CDVMarket.m
@@ -20,7 +20,7 @@
         
         CDVPluginResult *pluginResult;
         if (appId) {
-            NSString *url = [NSString stringWithFormat:@"itms-apps://itunes.com/app/%@", appId];
+            NSString *url = [NSString stringWithFormat:@"itms-apps://itunes.apple.com/app/%@", appId];
             [[UIApplication sharedApplication] openURL:[NSURL URLWithString:url]];
             
             pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
